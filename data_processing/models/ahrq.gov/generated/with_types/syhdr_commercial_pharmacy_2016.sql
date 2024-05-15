614df031b975735887cb02e0404c1a0c7bffecda
wesleycheung0 <wesley6d05@gmail.com>
2024-05-15 00:23:46 -0400

    Updated data model

--- data_processing/models/ahrq.gov/generated/with_types/syhdr_commercial_pharmacy_2016.sql
+++ data_processing/models/ahrq.gov/generated/with_types/syhdr_commercial_pharmacy_2016.sql
@@ -6,10 +6,10 @@ SELECT
     PERSON_WGHT::NUMERIC AS PERSON_WGHT,
     PHMCY_CLM_NUM::NUMERIC AS PHMCY_CLM_NUM,
     CLM_CNTL_NUM::NUMERIC AS CLM_CNTL_NUM,
-    LINE_NBR::VARCHAR,
+    LINE_NBR::VARCHAR AS LINE_NBR,
     FILL_DT::DATE AS FILL_DT,
-    SYNTHETIC_DRUG_ID::VARCHAR,
+    SYNTHETIC_DRUG_ID::VARCHAR AS SYNTHETIC_DRUG_ID,
     GENERIC_DRUG_NAME::VARCHAR AS GENERIC_DRUG_NAME,
     replace(replace(PLAN_PMT_AMT, '$', ''), ',', '')::FLOAT AS PLAN_PMT_AMT,
     replace(replace(TOT_CHRG_AMT, '$', ''), ',', '')::FLOAT AS TOT_CHRG_AMT
-FROM read_csv('/Users/me/data/syh_dr/syhdr_commercial_pharmacy_2016.CSV', header=True, null_padding=true)
+FROM read_csv('~/data/syh_dr/syhdr_commercial_pharmacy_2016.CSV', header=True, null_padding=true, types={ 'PERSON_ID': 'UBIGINT', 'PERSON_WGHT': 'NUMERIC', 'PHMCY_CLM_NUM': 'NUMERIC', 'CLM_CNTL_NUM': 'NUMERIC', 'LINE_NBR': 'VARCHAR', 'FILL_DT': 'DATE', 'SYNTHETIC_DRUG_ID': 'VARCHAR', 'GENERIC_DRUG_NAME': 'VARCHAR', 'PLAN_PMT_AMT': 'FLOAT', 'TOT_CHRG_AMT': 'FLOAT' }, ignore_errors=true)
--- data_processing/models/ahrq.gov/generated/with_types/syhdr_medicare_provider_2016.sql
+++ data_processing/models/ahrq.gov/generated/with_types/syhdr_medicare_provider_2016.sql
@@ -1,9 +1,9 @@
--- SQL model for syhdr_medicare_provider_2016.csv
+-- SQL model for syhdr_medicare_provider_2016.CSV
 {{ config(materialized='external', location=var('output_path') + '/' + this.name + '.parquet') }}
 
 SELECT
-    Facility_ID::VARCHAR,
-    Prvdr_Ctgry_Cd::VARCHAR,
-    Prvdr_Ownrshp_Cd::VARCHAR,
-    Prvdr_Prtcptn_Cd::VARCHAR
-FROM read_csv('/Users/me/data/syh_dr/syhdr_medicare_provider_2016.csv', header=True, null_padding=true)
+    Facility_ID::VARCHAR AS Facility_ID,
+    Prvdr_Ctgry_Cd::VARCHAR AS Prvdr_Ctgry_Cd,
+    Prvdr_Ownrshp_Cd::VARCHAR AS Prvdr_Ownrshp_Cd,
+    Prvdr_Prtcptn_Cd::VARCHAR AS Prvdr_Prtcptn_Cd
+FROM read_csv('~/data/syh_dr/syhdr_medicare_provider_2016.CSV', header=True, null_padding=true, types={ 'Facility_ID': 'VARCHAR', 'Prvdr_Ctgry_Cd': 'VARCHAR', 'Prvdr_Ownrshp_Cd': 'VARCHAR', 'Prvdr_Prtcptn_Cd': 'VARCHAR' }, ignore_errors=true)
