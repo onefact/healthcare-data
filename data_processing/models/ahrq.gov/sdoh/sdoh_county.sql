0873ac7957264f27f3ffe21ef06f25e4aa00f745
chrisgebert <cj@chrisgebert.org>
2024-05-24 14:47:24 -0400

    add sdoh models

---
+++ data_processing/models/ahrq.gov/sdoh/sdoh_county.sql
@@ -0,0 +1,34 @@
+{{ config(
+        materialized = 'external',
+        location = '../data/sdoh_county.parquet'
+    )
+}}
+
+{% set sdoh_county_years = [
+    '2009', '2010', '2011', '2012', '2013', '2014',
+    '2015', '2016', '2017', '2018', '2019', '2020'] %}
+
+{% set relations = [] %}
+{% for sdoh_county_year in sdoh_county_years -%}
+{% do relations.append(source('social_determinants_of_health', 'sdoh_county_' ~ sdoh_county_year)) %}
+{% endfor %}
+
+with union_unpivot as (
+
+    {% for relation in relations %}
+    unpivot {{ relation }}
+    on columns(* exclude (year, countyfips, statefips, state, county, region, territory))
+    into
+        name survey_variable_name
+        value survey_score
+
+    {% if not loop.last %} union all {% endif -%}
+    {% endfor %}
+)
+
+select
+    {{ dbt_utils.generate_surrogate_key(
+        ['year', 'countyfips', 'county']
+    ) }} as sdoh_county_key,
+    *
+from union_unpivot
---
+++ data_processing/models/ahrq.gov/sdoh/sdoh_tract.sql
@@ -0,0 +1,32 @@
+{{ config(
+        materialized = 'external',
+        location = '../data/sdoh_tract.parquet') }}
+
+{% set sdoh_tract_years = [
+    '2009', '2010', '2011', '2012', '2013', '2014',
+    '2015', '2016', '2017', '2018', '2019', '2020'] %}
+
+{% set relations = [] %}
+{% for sdoh_tract_year in sdoh_tract_years -%}
+{% do relations.append(source('social_determinants_of_health', 'sdoh_tract_' ~ sdoh_tract_year)) %}
+{% endfor %}
+
+with union_unpivot as (
+
+    {% for relation in relations %}
+    unpivot {{ relation }}
+    on columns(* exclude (year, tractfips, countyfips, statefips, state, county, region, territory))
+    into
+        name survey_variable_name
+        value survey_score
+
+    {% if not loop.last %} union all {% endif -%}
+    {% endfor %}
+)
+
+select
+    {{ dbt_utils.generate_surrogate_key(
+        ['year', 'tractfips', 'countyfips', 'county']
+    ) }} as sdoh_county_key,
+    *
+from union_unpivot
